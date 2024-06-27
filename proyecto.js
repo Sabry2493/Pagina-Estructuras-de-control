cfa890e53a36c2ba99f2f78c1a338c4a5d5ceb64
Sabry2493 <114948522+Sabry2493@users.noreply.github.com>
2024-06-27 16:56:35 -0300

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -35,3 +35,81 @@ document.getElementById('condicion').addEventListener('mouseenter',function(){
     fila3I.scrollIntoView({ behavior: 'smooth' });
 });
 
+//PARA VALIDAR FORMULARIO
+function Valida(){
+    //variables para tomar los datos de los input del formulario
+  
+    var nomInput=document.getElementById('nom');
+    var apeInput=document.getElementById('ape');
+    var emailInput=document.getElementById('email');
+    var telInput=document.getElementById('tel');
+    var textarea=document.getElementById('textArea');
+    divError=document.getElementById('Error');
+    divConfirmacion=document.getElementById('Confirmacion');
+    var formularioC=document.getElementById('formularioContacto');
+    var isValid = true;//booleano que utilizo para que si todo es valido retorne true sino false
+
+    divError.innerHTML = '';//div donde vere los carteles de error
+
+    if(nomInput.value==''){
+        nomInput.style.backgroundColor='rgba(252, 124, 124, 0.527)';
+        divError.innerHTML+='<span style="color: red;">*</span>El nombre es obligatorio <br>';
+        divError.scrollIntoView({ behavior: 'smooth' });
+        isValid=false;
+    }else if(nomInput.value.length < 3){//si el tamaño(length) del valor(value) del string(nomInput) es <3...
+        divError.innerHTML+='El nombre debe tener al menos 3 caracteres<br>';
+        divError.scrollIntoView({behavior:'smooth'});
+        isValid=false;//el booleano cambia y sera false
+    }else {
+       nomInput.style.backgroundColor='white'; // Restaurar color de fondo si el campo no está vacío
+    }
+
+    if(apeInput.value==''){
+        apeInput.style.backgroundColor='rgba(252, 124, 124, 0.527)';
+        divError.innerHTML+='<span style="color: red;">* </span>El apellido es obligatorio <br>';
+        divError.scrollIntoView({ behavior: 'smooth' });
+        isValid=false;
+    }else if (apeInput.value.length < 3) {//si el tamaño(length) del valor(value) del string(apeInput) es <3...
+        divError.innerHTML += 'El apellido debe tener al menos 3 caracteres<br>';
+        divError.scrollIntoView({behavior:'smooth'});
+        isValid = false;
+    } else {
+        apeInput.style.backgroundColor='white'; // Restaurar color de fondo si el campo no está vacío
+    }
+
+    if(emailInput.value==''&& telInput.value == '' ){
+        emailInput.style.backgroundColor='rgba(252, 124, 124, 0.527)';
+        divError.innerHTML+='<span style="color: red;">* </span>El email es obligatorio si no se registra un telefono.Ingrese un número de teléfono o email para comunicarnos<br>';
+        divError.scrollIntoView({ behavior: 'smooth' });
+        isValid=false;
+    //si email no esta vacio y el valor no incluye @ o no termina con .com entonces.... usando includes y endsWith
+    }else if (emailInput.value != '' && (!emailInput.value.includes('@') || !emailInput.value.endsWith('.com'))) {
+        divError.innerHTML += 'El e-mail debe tener un formato válido y finalizar en .com<br>';
+        divError.scrollIntoView({behavior:'smooth'});
+        isValid = false;
+    } 
+    else {
+        emailInput.style.backgroundColor='white'; // Restaurar color de fondo si el campo no está vacío
+    }
+    
+    if(textarea.value==''){
+        textarea.style.backgroundColor='rgba(252, 124, 124, 0.527)';
+        divError.innerHTML+='<span style="color: red;">* </span>Por favor comentenos brevemente en que necesita que la ayudemos<br> <br>';
+        divError.scrollIntoView({ behavior: 'smooth' });
+        isValid=false;
+    } else {
+        textarea.style.backgroundColor='white'; // Restaurar color de fondo si el campo no está vacío
+    } 
+    // Prevenir el envío del formulario si hay errores de validación
+    //si isValid no es true=false prevenir el envio del formulario
+    if (!isValid) {
+        event.preventDefault();
+    }else{
+        divConfirmacion.innerHTML='Envio exitoso, pronto nos pondremos en contacto.Gracias<br>';
+        divConfirmacion.scrollIntoView({behavior:'smooth'});
+        formularioC.reset();
+    }  
+     
+   return isValid; 
+}
+
